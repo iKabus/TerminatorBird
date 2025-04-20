295756b0449310a1ec10916d4a5920c20fdee3ee
Nikolay Semenov <164047031+iKabus@users.noreply.github.com>
2025-04-20 20:25:57 +0300

    PoolNext

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -10,36 +11,74 @@ public class PlayerMover : MonoBehaviour
     [SerializeField] private float _minRotationZ;
 
     private Vector3 _startPosition;
+
     private Rigidbody2D _rigidbody2D;
+
     private Quaternion _maxRotation;
     private Quaternion _minRotation;
 
-    private void Start()
+    private Coroutine _coroutine;
+
+    private Transform _chachedTransform;
+
+    private void Awake()
     {
-        _startPosition = transform.position;
+        _chachedTransform = transform;
+
         _rigidbody2D = GetComponent<Rigidbody2D>();
-
-        _maxRotation = Quaternion.Euler(0, 0, _maxRotationZ);
-        _minRotation = Quaternion.Euler(0, 0, _minRotationZ);
-
-        Reset();
     }
 
-    private void Update()
+    private void Start()
     {
-        transform.rotation = Quaternion.Lerp(transform.rotation, _minRotation, _rotationSpeed * Time.deltaTime);
+        _startPosition = _chachedTransform.position;
+
+        InitializeRotations();
+        Reset();
     }
 
     public void MoveUp()
     {
         _rigidbody2D.linearVelocity = new Vector2(_speed, _tapForce);
-        transform.rotation = _maxRotation;
+
+        _chachedTransform.rotation = _maxRotation;
+
+        if (_coroutine == null == false)
+        {
+            StopCoroutine(_coroutine);
+        }
+
+        _coroutine = StartCoroutine(RotateToMinAngle());
     }
 
     public void Reset()
     {
+        if (_coroutine == null == false)
+        {
+            StopCoroutine(_coroutine );
+
+            _coroutine = null;
+        }
+
         transform.position = _startPosition;
         transform.rotation = Quaternion.identity;
         _rigidbody2D.linearVelocity = Vector2.zero;
+    }
+
+    private void InitializeRotations()
+    {
+        _maxRotation = Quaternion.Euler(0, 0, _maxRotationZ);
+        _minRotation = Quaternion.Euler(0, 0, _minRotationZ);
+    }
+
+    private IEnumerator RotateToMinAngle()
+    {
+        while(Quaternion.Angle(_chachedTransform.rotation, _minRotation) > 0.1f)
+        {
+            _chachedTransform.rotation = Quaternion.Lerp(_chachedTransform.rotation, _minRotation, _rotationSpeed * Time.deltaTime);
+
+            yield return null;
+        }
+
+        _chachedTransform.rotation = _minRotation;
     }
 }
